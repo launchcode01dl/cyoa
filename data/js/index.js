736453797f01c10f8a960f1b37c5dd968a4a39c9
Gaurav Arora <arora_gaurav97@protonmail.com>
2022-09-12 18:21:45 +0530

    update

--- data/js/index.js
+++ data/js/index.js
@@ -15,10 +15,17 @@ $('.btn').click(function(){
       $(this).addClass("active").siblings().removeClass("active");
     });
 
-  function newSite(value) {   
-    document.getElementById('myIframe').src = "data/" + value + ".html";
-  }
-  function githubSite() {   
-    var target= "https://github.com/launchcode01dl/cyoa";
-    window.open(target, '_blank')
-  }
+function commonSite(name,size) { 
+  var i = document.getElementById('myIframe');
+  i.src = "data/common.html";
+  i.name = 'jsons/' + name + '.json';
+  i.size = size*(1024**2);
+
+}
+function customSite(value) { 
+  document.getElementById('myIframe').src = "data/" + value + ".html";
+}
+function githubSite() {   
+  var target= "https://github.com/launchcode01dl/cyoa";
+  window.open(target, '_blank')
+}
