5c45eaf1c10c1075061012b959157974f2d624f4
Gaurav Arora <arora_gaurav97@protonmail.com>
2022-09-15 19:07:39 +0530

    added new cyoas

--- sidebar.js
+++ sidebar.js
@@ -12,20 +12,24 @@ const mainList = [
     {name: "Punch", index: "11"},
     {name: "Serenity", index: "12"},
     {name: "Traveller", index: "13"},
-    {name: "Yunzhong Hou", index: "14"}
-  ]
+    {name: "Yunzhong Hou", index: "14"},
+    {name: "Aldar", index: "15"},
+    {name: "Maximus", index: "16"},
+    {name: "Om1cr0n", index: "17"},
+    {name: "Graev", index: "18"}
+]
   
-  const childList =[
+const childList =[
     {index: "1", path: 'custom/brigadewormV6/index', size: '0', name: "Worm V6"},
     {index: "2", path: 'custom/glyphwormV6/index', size: '0', name: "Worm V6"},
-    {index: "2", path: 'glyphmha', size: '1.1', name: "MHA"},
-    {index: "3", path: 'goodworldmysticmartialarts', size: '3.6', name: "Mystic Martial Arts"},
+    {index: "2", path: 'glyphmha', size: '2', name: "MHA"},
+    {index: "3", path: 'goodworldmysticmartialarts', size: '4', name: "Mystic Martial Arts"},
     {index: "4", path: 'infaeraharrypotter', size: '1', name: "Harry Potter"},
     {index: "4", path: 'infaeraopisekai', size: '6', name: "OP Isekai"},
     {index: "5", path: 'kondorhp', size: '10', name: "Harry Potter"},
     {index: "6", path: 'lennkazjedi', size: '75', name: "SW Jedi"},
-    {index: "6", path: 'lennkazsithlord', size: '3.5', name: "SW Sith Lord"},
-    {index: "6", path: 'lennkazwarlord', size: '15.5', name: "SW Warlord"},
+    {index: "6", path: 'lennkazsithlord', size: '4', name: "SW Sith Lord"},
+    {index: "6", path: 'lennkazwarlord', size: '16', name: "SW Warlord"},
     {index: "7", path: 'ltouroumovwormv6', size: '45', name: "Worm V6"},
     {index: "10", path: 'pasadenalg', size: '21', name: "Living God"},
     {index: "10", path: 'pasadenawormv3', size: '12', name: "Worm V3"},
@@ -33,5 +37,15 @@ const mainList = [
     {index: "11", path: 'punchnaruto', size: '30', name: "Naruto"},
     {index: "12", path: 'serenityhp', size: '57', name: "Harry Potter"},
     {index: "13", path: 'travellerpsychic', size: '13', name: "Psychic"},
-    {index: "14", path: 'custom/YunzhongHouLuminarium/index', size: '0', name: "Luminarium"}
-  ]
+    {index: "14", path: 'custom/YunzhongHouLuminarium/index', size: '0', name: "Luminarium"},
+    {index: "9", path: 'pixelgmsrwby1.2', size: '4', name: "RWBY 1.2"},
+    {index: "9", path: 'pixelgmsrwby1.1', size: '10', name: "RWBY 1.1"},
+    {index: "9", path: 'pixelgmsfatebeta', size: '5', name: "Fate Beta"},
+    {index: "15", path: 'aldartech', size: '2', name: "Tech Transdimensional"},
+    {index: "13", path: 'travellerworm', size: '13', name: "Worm"},
+    {index: "8", path: 'hikillerpcxenon', size: '2', name: "Power Creator Xenon"},
+    {index: "16", path: 'custom/maximusworm/index', size: '0', name: "Worm"},
+    {index: "17", path: 'Om1cr0nrwby', size: '4', name: "RWBY"},
+    {index: "18", path: 'graevmm', size: '15', name: "Multiverse Mage"},
+    {index: "17", path: 'Om1cr0ndemigod', size: '33', name: "Demigod"}
+]
